48b3d9c023a2e1674c69adcd02f7a4170e078adc
mandian <mandian@users.noreply.github.com>
2022-12-31 21:20:46 +0000

    update check-update.sh

--- check-update.sh
+++ check-update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-#curl "https://github.com/BelledonneCommunications/postquantumcryptoengine/tags" 2>/dev/null |grep "tag/" |sed -e 's,.*tag/,,;s,\".*,,;' |grep -E '^[0-9.]+$' |grep -v '[0-9][0-9][0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]' |sort -V |tail -n1
-curl -s "https://gitlab.linphone.org/BC/public/postquantumcryptoengine/-/tags"  |grep "tags/" |sed -e 's,.*tags/,,;s,\".*,,;' |grep -E '^[0-9.]+$' |grep -v '[0-9][0-9][0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]' |sort -V |tail -n1
+#curl -s "https://github.com/BelledonneCommunications/postquantumcryptoengine/tags" |grep "tag/" |sed -e 's,.*tag/,,;s,\".*,,;' |grep -E '^[0-9.]+$' |grep -v '[0-9][0-9][0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]' |sort -V |tail -n1
+curl -s "https://gitlab.linphone.org/BC/public/postquantumcryptoengine/-/tags" |grep "tags/" |sed -e 's,.*tags/,,;s,\".*,,;' |grep -E '^[0-9.]+$' |grep -v '[0-9][0-9][0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]' |sort -V |tail -n1
 
